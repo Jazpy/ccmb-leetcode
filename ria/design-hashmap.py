219c76a4f96f852e1c19f50c880ea1a1ac25613a
Ria Vinod <rvinod@vscode1.oscar.ccv.brown.edu>
2023-10-10 18:32:44 -0400

    easy :D

---
+++ ria/design-hashmap.py
@@ -0,0 +1,58 @@
+class MyHashMap(object):
+
+    def __init__(self):
+        self.map = []
+        
+
+    def put(self, key, value):
+        """
+        :type key: int
+        :type value: int
+        :rtype: None
+        """
+        key_present = False
+        for pair in self.map:
+            if key == pair[0]:
+                pair[1] = value
+                key_present = True
+        if key_present is False:
+            self.map.append([key, value])
+        
+        
+
+    def get(self, key):
+        """
+        :type key: int
+        :rtype: int
+        """
+
+        key_present = False
+        for pair in self.map:
+            if key == pair[0]:
+                return pair[1]
+        return -1
+
+        
+
+    def remove(self, key):
+        """
+        :type key: int
+        :rtype: None
+        """
+        i = 0
+        for pair in self.map:
+            if key == pair[0]:
+                self.map.pop(i)
+            i+=1
+        
+        
+
+
+# Your MyHashMap object will be instantiated and called as such:
+# obj = MyHashMap()
+# obj.put(key,value)
+# param_2 = obj.get(key)
+# obj.remove(key)
+
+# 2247ms
+# 16.54MB
